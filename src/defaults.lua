63d46e2e490078bf3e9d63f5817cb6107f036b9e
Georgi Valkov <georgi.t.valkov@gmail.com>
2013-08-31 18:42:16 +0200

    [wip]

---
+++ src/defaults.lua
@@ -0,0 +1,22 @@
+default_style = {
+   bgcolor = {255, 0, 0},
+   fgcolor = {0, 255, 0},
+   height = 'auto',
+
+   -- text options
+   font = 'Sans Italic 12',
+   font_style = 'normal', -- 'oblique', 'italic',
+   font_weight = 'ultralight', -- 'light', 'normal', 'bold', 'ultrabold', 'heavy', number
+   font_variant = 'normal', -- 'smallcaps'
+   font_stretch = 'ultracondensed', -- 'extracondensed', 'condensed', 'semicondensed', 'normal', 'semiexpanded', 'expanded', 'extraexpanded', 'ultraexpanded',
+   underline = 'none', -- 'single', 'double', 'low', 'error',
+   underline_color = '#333333',
+   rise = 1,
+   strikethrough = false,
+   strikethrough_color = '#FF00FF',
+   letter_spacing = 200,
+   gravity = 'south', -- 'east', 'north', 'west', 'auto',
+   gravity_hint ='natural', -- 'strong', 'line',
+
+   -- 
+}
